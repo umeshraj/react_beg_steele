c7f26593856f8a4ab6d30125f58100d5ca62805d
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-06-25 08:34:39 -0400

    Multiple items in return

--- 01_react_w_html/script.js
+++ 01_react_w_html/script.js
@@ -1,5 +1,10 @@
 const Dog = () => {
-  return <h1>I AM DOG</h1>;
+  return (
+    <div>
+      <h1>I AM DOG</h1>
+      <h2>woof woof</h2>
+    </div>
+  );
 };
 
 ReactDOM.render(<Dog />, document.getElementById("root"));
