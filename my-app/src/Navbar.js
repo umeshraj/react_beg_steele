d841a5f93888af9ea01ae4aadfe999382a0cc3c6
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-06-26 06:44:03 -0400

    Making our own navbar component

---
+++ my-app/src/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Contact</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
