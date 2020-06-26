7575fdde6c40fb61c356c620e5ac4ad0bf1927ab
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-06-26 07:59:01 -0400

    Props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./Navbar";
+import Greeter from "./Greeter";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Greeter name="Karen" />
+      <Greeter name="Tammy" />
       <h1>Hello from App!</h1>
     </div>
   );
---
+++ my-app/src/Greeter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Greeter(props) {
+  return <h1>Hi there, {props.name}</h1>;
+}
+
+export default Greeter;
