a2204b713ba7c7e97f1c129f21a0d372c0070da4
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-06-27 09:45:25 -0400

    Using inline and css styles

--- my-app/src/MoodToggler.js
+++ my-app/src/MoodToggler.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
+import "./MoodToggler.css";
 
 function MoodToggler() {
   const [isHappy, setIsHappy] = useState(true);
   const toggleIsHappy = () => {
     setIsHappy(!isHappy);
   };
-  return <h3 onClick={toggleIsHappy}>{isHappy ? ":)" : ":("}</h3>;
+  const styles = { color: isHappy ? "green" : "red" };
+  return (
+    <h3 className="MoodToggler" style={styles} onClick={toggleIsHappy}>
+      {isHappy ? ":)" : ":("}
+    </h3>
+  );
 }
 
 export default MoodToggler;
