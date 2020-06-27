4f54c4821c1702b66025a19a1bd2fb70e734f950
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-06-27 08:39:01 -0400

    Second state to flip happy face

--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -2,10 +2,15 @@ import React, { useState } from "react";
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [isHappy, setIsHappy] = useState(true);
+  const toggleIsHappy = () => {
+    setIsHappy(!isHappy);
+  };
   return (
     <div>
       <h2>{count}</h2>
       <button onClick={() => setCount(count + 1)}>+1</button>
+      <h3 onClick={toggleIsHappy}>{isHappy ? ":)" : ":("}</h3>
     </div>
   );
 }
