10a81f375b4ad4a260bada37487987f085bc41df
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-06-28 09:49:49 -0400

    Changing prop inside component does not work

--- my-app/src/Cart.js
+++ my-app/src/Cart.js
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import CartItem from "./CartItem";
 
 function Cart({ initialItems }) {
-  const grandTotal = initialItems
+  const [items, setItems] = useState(initialItems);
+  const grandTotal = items
     .reduce((total, item) => total + item.qty * item.price, 0)
     .toFixed(2);
   return (
     <div className="Cart">
       <h1>I AM CART</h1>
       <div className="Cart-items">
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <CartItem key={item.id} {...item} />
         ))}
       </div>
--- my-app/src/CartItem.js
+++ my-app/src/CartItem.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 function CartItem({ id, name, price, qty }) {
+  const addOne = () => {
+    qty += 1;
+  };
   return (
     <div className="CartItem">
       <div>{name}</div>
       <div>${price}</div>
-      <div>{qty}</div>
+      <div>
+        <button>-</button>
+        {qty}
+        <button onClick={addOne}>+</button>
+      </div>
+
       <div>Total: ${qty * price}</div>
     </div>
   );
